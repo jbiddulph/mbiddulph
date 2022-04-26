a335edcc4a94a43bf08931733011e922ec3560c0
jbiddulph <john.mbiddulph@gmail.com>
2022-04-26 23:50:11 +0100

    wider table shorter category

--- resources/views/home-none-exhibit.blade.php
+++ resources/views/home-none-exhibit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-md-3">
 
                 <div class="row">
                     <div class="col-md-12">
@@ -98,7 +98,7 @@
                 </div>
 
         </div>
-        <div class="col-md-8">
+        <div class="col-md-9">
             @if(count($errors) > 0)
                 <div class="alert alert-danger">
                     <ul>
