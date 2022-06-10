54f6686a3ed153cf65915eefe7ace39628524c3e
jbiddulph <john.mbiddulph@gmail.com>
2022-06-10 17:28:43 +0100

    styling download from app store logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,6 +27,11 @@
                 </div>
             </div>
             <div class="col-md-4 col-12 artistinfo">
+                <div>
+                    <a href="https://apps.apple.com/us/app/mbiddulph/id1627696444" target="_blank">
+                        <img src="https://melvynbiddulph.co.uk/images/old-dog.jpg" width="100%" alt="Download on the app store" />
+                    </a>
+                </div>
                 <h2>About Melvyn Biddulph</h2>
                 <ul>
                     <li>BORN IN ROCHDALE 1949</li>
@@ -51,11 +56,6 @@
                     <li><i class="fab fa-facebook-square"></i>&nbsp;&nbsp;<a href="https://www.facebook.com/melvyn.biddulph">Facebook Profile</a></li>
                     <li><i class="fas fa-phone-square-alt"></i>&nbsp;&nbsp;<a href="tel:07534226701">07534 226701</a></li>
                 </ul>
-                <div>
-                    <a href="https://apps.apple.com/us/app/mbiddulph/id1627696444" target="_blank">
-                        <img src="https://melvynbiddulph.co.uk/images/old-dog.jpg" width="100%" alt="Download on the app store" />
-                    </a>
-                </div>
                 <!-- Modal -->
                 <div class="modal fade" id="readMore" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
